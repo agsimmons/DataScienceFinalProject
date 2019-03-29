8d68b58be07e7d996298a8cf69710eb6f2355c26
Andrew Simmons <agsimmons0@gmail.com>
2019-03-29 15:51:30 -0400

    Remove Pickle path from config.py

--- config.py
+++ config.py
@@ -5,5 +5,3 @@ PROJECT_DIR = pathlib.Path(__file__).parent
 DATA_DIR = PROJECT_DIR / pathlib.Path('data')
 
 DATA_DB = PROJECT_DIR / 'data.sqlite3'
-
-ALL_DATA_PICKLE = PROJECT_DIR / 'all_data.pickle'
